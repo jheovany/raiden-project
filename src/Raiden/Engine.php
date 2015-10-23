c7e2e83bc6fe80b9856550df928471b9d66e6de3
Jheovany Menjivar <jheovany.menjivarg@gmail.com>
2015-10-22 20:12:38 -0600

    El metodo find() sera utilizado con los filtros aplicados.

--- src/Raiden/Engine.php
+++ src/Raiden/Engine.php
@@ -120,18 +120,18 @@ class Engine {
 
 	public function findByPK( $id ) {
 
-		return $this->find($id);
+		return $this->findWhere($id);
 	}	
 
 	public function findByFK($cond) {
 		
-		return $this->find($cond, true);
+		return $this->findWhere($cond, true);
 	}
 
-	public function findWhere() {
+	public function find() {
 		
 		if ($this->filter) {
-			return $this->find($this->filter->getSqlFilter(), true);
+			return $this->findWhere($this->filter->getSqlFilter(), true);
 		} else {
 			return $this->findAll();
 		}
@@ -139,10 +139,10 @@ class Engine {
 
 	public function findAll($limit = 100) {
 
-		return $this->find($limit, false, true);
+		return $this->findWhere($limit, false, true);
 	}	
 
-	public function find( $cond, $isWhere = false, $isLimit = false ) {
+	public function findWhere( $cond, $isWhere = false, $isLimit = false ) {
 
 		$this->fetchedObjects = null;
 
